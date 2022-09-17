0ada02b487cd0314015831b3be079200f65c9482
Isabella Bresciani <isabellabresciani@MacBook-Air-de-Isabella.local>
2022-09-17 19:47:25 -0300

    añadiendo fetch

--- js/script2.js
+++ js/script2.js
@@ -1,53 +1,7 @@
-class Productos{
-    constructor(imagen,titulo,precio,id){
-        this.imagen=imagen
-        this.titulo=titulo
-        this.precio=precio
-        this.id=id
-    }
-}
-
-const prod1 = new Productos("../footage/short.jpeg", "Short maternal ONA","$4999","1")
-const prod2 = new Productos("../footage/pantalon.jpeg", "Pantalon maternal bianca","$9300","2")
-const prod3 = new Productos("../footage/palazo.jpeg", "Palazzo maternal malena","$8300","3")
-const prod4 = new Productos("../footage/camiseta.jpeg", "Camiseta Sarah","$7000","4")
-const prod5 = new Productos("../footage/buzo.png", "Buzo maternal Sofia","$11000","5")
-const prod6 = new Productos("../footage/remera.png", "Remera LORE","$5000","6")
-
-const productos = document.getElementById("productos")
-
-
-
-const sectionProductos = []
-
-
-async function loadProds() {
-    const response = await fetch('../json/productos.json');
-    const prods = await response.json();
-    prods.forEach(prod => {
-        console.log(prod)
-        sectionProductos.push(prod)
-    })
-}
-
-loadProds()
-
-
-sectionProductos.forEach((prod) => {
-    productos.innerHTML += `
-    <article class="blog">
-        <img src=${prod.imagen}>
-        <h4>${prod.titulo}</h4>
-        <p>${prod.precio}</p>
-        <button class="buttonCarrito" id="carrito${prod.id}">agregar al carrito</button>
-    </article>
-    `
-})
-console.log(sectionProductos)
-
 let button
 const botones = []
 let compra = []
+//creo la key carrito en el local si no existe, si existe obtengo su valor
 
 if (localStorage.getItem("Carrito")){
     compra = JSON.parse(localStorage.getItem("Carrito"))
@@ -55,24 +9,50 @@ if (localStorage.getItem("Carrito")){
     localStorage.setItem("Carrito",JSON.stringify(compra))
 }
 
-for (i=1;i<=sectionProductos.length;i++){
-    button = document.getElementById(`carrito${i}`)
-    botones.push(button)
+
+//Obtengo el div donde voy a insertar los productos
+const divProductos = document.getElementById("productos")
+
+//traigo los productos del JSON
+const traerProductos = async () => {
+    const response = await fetch("../json/productos.json");
+    const prods = await response.json();
+    return prods
 }
 
-botones.forEach((button)=> (
-    button.addEventListener("click",()=>{
-        compra.push(sectionProductos[botones.indexOf(button)])
-        localStorage.setItem("Carrito",JSON.stringify(compra))
-        Swal.fire(
-            'Añadiste este producto al carrito de compras!',
-            'Felicidades!',
-            'success'
-        )
-    })
-))
+//Los mustro en el front y los guardo en otro array
+let sectionProductos = []
+traerProductos().then(productos =>{
+    sectionProductos = productos
+    console.log(sectionProductos)
+    console.log(sectionProductos.length)
+    productos.forEach((prod) => {
+        divProductos.innerHTML += `
+        <article class="blog">
+            <img src=${prod.imagen}>
+            <h4>${prod.titulo}</h4>
+            <p>${prod.precio}</p>
+            <button class="buttonCarrito" id="carrito${prod.id}">agregar al carrito</button>
+        </article>`})
+    for (i=1;i<=sectionProductos.length;i++){
+        button = document.getElementById(`carrito${i}`)
+        botones.push(button)
+    }
+    
+    botones.forEach((button)=> (
+        button.addEventListener("click",()=>{
+            compra.push(sectionProductos[botones.indexOf(button)])
+            localStorage.setItem("Carrito",JSON.stringify(compra))
+            Swal.fire(
+                'Añadiste este producto al carrito de compras!',
+                'Felicidades!',
+                'success'
+            )
+        })
+    ))
+})
 
-const carro = document.getElementById("carro")
+
 
 
 
